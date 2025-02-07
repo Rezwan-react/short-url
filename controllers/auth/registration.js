83f88658396d92d9c36502177b350f8d806c74ee
Rezwan-react <mdrazwanislam8@gmail.com>
2025-02-07 20:18:02 +0600

    registration done

--- controllers/auth/registration.js
+++ controllers/auth/registration.js
@@ -1,6 +1,8 @@
 const ValidateEmail = require("../../helpers/validateEmail");
 const validatePassword = require("../../helpers/validatePassword");
 const registrationSchema = require("../../modal/registrationSchema");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const registration = async (req, res) => {
 
@@ -13,29 +15,40 @@ const registration = async (req, res) => {
         if (!email) {
             return res.status(400).send("Enter your email")
         }
-    
-        if(!ValidateEmail(email)){
+
+        if (!ValidateEmail(email)) {
             return res.status(400).send("Email not invalid")
         }
-    
+
         if (!password) {
             return res.status(400).send("Enter your password")
         }
-    
+
         const validatePassResult = validatePassword(password)
         console.log(validatePassword(password));
-        
-        if(validatePassResult){
+
+        if (validatePassResult) {
             return res.status(400).send(validatePassResult)
         }
+
+        const existinguser = await registrationSchema.findOne({email})
         
-        const user = await registrationSchema({
-            userName, email, password
-        })
-        
-        user.save()
+        if(existinguser){
+            return res.status(400).send("Email already exist")
+        }
+
+        bcrypt.hash(password, saltRounds, function(err, hash) {
+            
+            const user =  registrationSchema({
+                userName, email, password: hash
+            })
     
-        res.send(user)
+            user.save()
+    
+            res.send("successful")
+            
+        });
+        
     } catch (error) {
         return res.status(400).send("Server side error please try again")
     }
